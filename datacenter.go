54010b2f12d3782d23883dc8e47fd5b5d36f5285
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2025-05-28 16:49:06 +0330

    refactor(datacenter): map hcloud.Datacenter to custom DatacenterResponse struct
    - Introduced DatacenterResponse struct to define a clean API output format
    - Added Location and ServerTypeId helper structs for nested response formatting
    - Replaced direct usage of *hcloud.Datacenter in handlers with the new response format
    - Implemented conversion functions for datacenter and server type mapping

--- datacenter.go
+++ datacenter.go
@@ -19,15 +19,68 @@ type DatacenterReadByNameArgs struct {
 	Name string `json:"name" jsonschema:"required,description=The datacenter name to be searched"`
 }
 
+type Location struct {
+	ID   int64  `json:"id" jsonschema:"required,description=The location id"`
+	Name string `json:"name" jsonschema:"required,description=The location name"`
+}
+
+type ServerTypeId struct {
+	ID int64 `json:"id" jsonschema:"required,description=The server type id"`
+}
+
+type DatacenterResponse struct {
+	ID                               int64          `json:"id" jsonschema:"required,description=The datacenter id"`
+	Name                             string         `json:"name" jsonschema:"required,description=The datacenter name"`
+	Description                      string         `json:"description" jsonschema:"description=The datacenter description"`
+	Location                         Location       `json:"location" jsonschema:"required,description=The location of the datacenter"`
+	SupportedServerTypes             []ServerTypeId `json:"supported_server_types" jsonschema:"description=List of supported server types"`
+	AvailableForMigrationServerTypes []ServerTypeId `json:"available_for_migration_server_types" jsonschema:"description=List of available for migration server types"`
+	AvailableServerTypes             []ServerTypeId `json:"available_server_types" jsonschema:"description=List of available server types"`
+}
+
+func toServerTypeIdList(serverTypes []*hcloud.ServerType) []ServerTypeId {
+	var result []ServerTypeId
+	for _, serverType := range serverTypes {
+		result = append(result, ServerTypeId{ID: serverType.ID})
+	}
+	return result
+}
+
+func toDatacenterResponse(d *hcloud.Datacenter) *DatacenterResponse {
+	if d == nil {
+		return nil
+	}
+
+	return &DatacenterResponse{
+		ID:          d.ID,
+		Name:        d.Name,
+		Description: d.Description,
+		Location: Location{
+			ID:   d.Location.ID,
+			Name: d.Location.Name,
+		},
+		SupportedServerTypes:             toServerTypeIdList(d.ServerTypes.Supported),
+		AvailableForMigrationServerTypes: toServerTypeIdList(d.ServerTypes.AvailableForMigration),
+		AvailableServerTypes:             toServerTypeIdList(d.ServerTypes.Available),
+	}
+}
+
 // DatacenterTools
 var datacenterTools = []Tool{
 	{
 		Name:        "get_all_datacenters",
 		Description: "Returns all Datacenters objects.",
 		Handler: func(_ NoArgs) (*mcpgolang.ToolResponse, error) {
-			return handleResponse(func() ([]*hcloud.Datacenter, error) {
+			return handleResponse(func() ([]*DatacenterResponse, error) {
 				result, err := client.Datacenter.All(context.Background())
-				return result, err
+				if err != nil {
+					return nil, err
+				}
+				var filtered []*DatacenterResponse
+				for _, d := range result {
+					filtered = append(filtered, toDatacenterResponse(d))
+				}
+				return filtered, nil
 			})
 		},
 		Restriction: RestrictionReadOnly,
@@ -36,9 +89,12 @@ var datacenterTools = []Tool{
 		Name:        "get_a_datacenter_by_id",
 		Description: "Retrieves a Datacenter by its ID. If the Datacenter does not exist, nil is returned.",
 		Handler: func(args DatacenterReadByIDArgs) (*mcpgolang.ToolResponse, error) {
-			return handleResponse(func() (*hcloud.Datacenter, error) {
+			return handleResponse(func() (*DatacenterResponse, error) {
 				result, _, err := client.Datacenter.GetByID(context.Background(), args.ID)
-				return result, err
+				if err != nil {
+					return nil, err
+				}
+				return toDatacenterResponse(result), nil
 			})
 		},
 		Restriction: RestrictionReadOnly,
@@ -47,9 +103,12 @@ var datacenterTools = []Tool{
 		Name:        "get_a_datacenter_by_name",
 		Description: "Retrieves a Datacenter by its Name. If the Datacenter does not exist, nil is returned.",
 		Handler: func(args DatacenterReadByNameArgs) (*mcpgolang.ToolResponse, error) {
-			return handleResponse(func() (*hcloud.Datacenter, error) {
+			return handleResponse(func() (*DatacenterResponse, error) {
 				result, _, err := client.Datacenter.GetByName(context.Background(), args.Name)
-				return result, err
+				if err != nil {
+					return nil, err
+				}
+				return toDatacenterResponse(result), nil
 			})
 		},
 		Restriction: RestrictionReadOnly,
